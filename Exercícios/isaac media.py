92832037a40821436008264d10717fbcb37fbf63
Joabe Soares Teixeira de Souza <joabe.soares1@gmail.com>
2022-12-12 09:17:24 -0300

    update isaac

--- Exercícios/isaac media.py
+++ Exercícios/isaac media.py
@@ -7,6 +7,6 @@ print('-=' * 20)
 for c in range(0, 4):
     nota.append(float(input(f'Nota {c+1}: ')))
 print('-=' * 20)
-print(f'O aluno {aluno} ficou a média em {materia}: {sum(nota)/4}')
+print(f'O aluno {aluno} ficou com média em {materia} de: {sum(nota)/4}')
 print('-=' * 20)
 
